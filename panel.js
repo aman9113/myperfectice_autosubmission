881b384ffeee1e2758360b49ad35613b7a00acb9
Aman Singh <92121302+aman9113@users.noreply.github.com>
2022-12-29 09:21:45 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,7 +1,7 @@
 
 /*
     Author: Aman Singh
-    Follow @aman9113 github @its__aman008 instagram
+    Follow  @its__aman008 instagram @aman9113 github
     Note:
     This script is free to use, do not pay anyone anything.
     To modify or redistribute, kindly follow the license agreement strictly.
